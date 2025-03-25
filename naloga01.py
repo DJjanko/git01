c127222b707610ded10ad5282feca008446f576e
DJjanko <jankladusek7@gmail.com>
2025-03-25 21:08:32 +0100

    Definicije funkcij

---
+++ naloga01.py
@@ -0,0 +1,29 @@
+from email.mime import image
+import numpy as np
+import cv2 as cv
+import time
+
+coordinates = []
+
+def click_event(event, x, y, flags, param):
+
+
+def doloci_barvo_koze(image, coordinatesS, coordinatesE):
+
+
+def zmanjsaj_sliko(image, sirina, visina):
+
+
+def is_between(value, lower, upper):
+
+
+def obdelaj_sliko_s_skatlami(image, sirina_skatle, visina_skatle, color):
+
+
+def prestej_piksle_z_barvo_koze(image,color):
+
+
+def display_frames(image, fps):
+
+
+if __name__ == '__main__':
