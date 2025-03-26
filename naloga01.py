0707d99b95eb371f7c9300e9afe4139c5a9e4959
DJjanko <jankladusek7@gmail.com>
2025-03-26 20:01:57 +0100

    click event, doloci barvo koze, zmanjsaj sliko

--- naloga01.py
+++ naloga01.py
@@ -5,13 +5,73 @@ import time
 
 coordinates = []
 
+#click event za image, da označi kvadrat in ko se levi klik zgodi se označi na sliki beli kvadratek, koordinate shrani v polje
 def click_event(event, x, y, flags, param):
+    global drawing, selected
+    if event == cv.EVENT_LBUTTONDOWN:
+        # print(x, ' ' ,y)
+        coordinates.append((x, y))
+        font = cv.FONT_HERSHEY_SIMPLEX
+        # cv.putText(image, str(x) + ',' + str(y),(x,y),font,0.3,(255,0,0),2)
+        cv.imshow('image', image)
+
+    if event == cv.EVENT_LBUTTONUP:
+        # print(x, ' ', y)
+        selected = False
+        coordinates.append((x, y))
+        img_copy = image.copy()
+        font = cv.FONT_HERSHEY_SIMPLEX
+        # cv.putText(image, str(x) + ',' + str(y), (x, y), font, 0.3, (255, 0, 0), 2)
+
+        cv.rectangle(img_copy, (coordinates[0]), (x, y), (255, 255, 255), -1)
+        cv.addWeighted(img_copy, 0.5, image, 0.5, 0, image)
+        cv.imshow('image', image)
 
 
+#iz prej označenega okvirja izračuna povprečje vsakega kanala barve in vrne vrednosti kot numpay array, nižja in višja dovoljena vrednost
 def doloci_barvo_koze(image, coordinatesS, coordinatesE):
+    global coordinates
+    tempR = 0.0
+    tempG = 0.0
+    tempB = 0.0
+
+    xS, yS = coordinatesS
+    xE, yE = coordinatesE
+    xS, xE, yS, yE = int(xS), int(xE), int(yS), int(yE)
+
+    print("xS:", xS, "xE:", xE, "yS:", yS, "yE:", yE)  # Debugging print statement
+
+    counter = 0
+
+    for i in range(xS, xE):
+        for j in range(yS, yE):
+            print("i:", i, "j:", j)  # Debugging print statement
+            counter = counter + 1
+            tempB = image[j, i, 0] + tempB
+            tempG = image[j, i, 1] + tempG
+            tempR = image[j, i, 2] + tempR
+
+    tempR = tempR / counter
+    tempG = tempG / counter
+    tempB = tempB / counter
+
+    result = np.array([[tempB - 25, tempG - 25, tempR - 25], [tempB + 25, tempG + 25, tempR + 25]]).reshape(2, 3)
+
+    return result
 
 
+#zmanjsa sliko glede na aspect ratio, da ne pokvari slike in vrne sliko
 def zmanjsaj_sliko(image, sirina, visina):
+    imgVisina, imgSirina = image.shape[:2]
+
+    aspect_ratio = imgSirina / imgVisina
+
+    if imgSirina > imgVisina:
+        visina = int(sirina / aspect_ratio)
+    else:
+        sirina = int(visina * aspect_ratio)
+    #print("Sirina: ", sirina, ' ', "Visina: ", visina)
+    return cv.resize(image, (sirina, visina))
 
 
 def is_between(value, lower, upper):
